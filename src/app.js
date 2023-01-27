e10023f922eb75da369967bebcfc082e50ac7162
Rayane Pimentel <radharayg@gmail.com>
2023-01-27 13:33:14 -0300

    hall

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const path = require('path');
 const indexRouter = require('./routes/rotas');
 
 app.use(bodyParser.json());
+app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.resolve(__dirname, './public/img')))
 
 app.use('/', indexRouter);
 
