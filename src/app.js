517cb1f7ee8224a1707dc8bdc356ee441c09a1a8
Rayane Pimentel <radharayg@gmail.com>
2023-01-28 12:16:01 -0300

    atualizando arquivos

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const path = require('path');
 const indexRouter = require('./routes/rotas');
 
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('publico'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.resolve(__dirname, './public/img')))
+app.use(express.static(path.join(__dirname, 'publico')));
+app.use('/images', express.static(path.resolve(__dirname, './publico/img')))
 
 app.use('/', indexRouter);
 
