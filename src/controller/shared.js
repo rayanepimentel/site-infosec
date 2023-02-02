6ce4d502bb436d54dc6a10fde11719acdc530ed7
Rayane Pimentel <radharayg@gmail.com>
2023-02-02 20:16:09 -0300

    add shared linkedin

--- src/controller/shared.js
+++ src/controller/shared.js
@@ -1,6 +1,11 @@
 exports.sharedTw = function(texto) {
-  console.log('aquooo', texto)
   const url ="https://twitter.com/share?ref_src=twsrc%5Etfw" 
   const dataText= texto;
   return `<a href=${url} class="twitter-share-button" data-text="${dataText}" data-lang="pt" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
+}
+
+exports.sharedLink = function(urlPage) {
+  const url = urlPage;
+  const linkedin = 'https://www.linkedin.com/sharing/share-offsite/';
+  return `<a href='${linkedin}?url=https://infosec-iniciante.onrender.com${url}', target="_blank" style="color: #FCFCFB; background-color: #477EAE; border-radius: 5px;"><i class="fa fa-linkedin fa-fw"></i></a>`;
 }
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const tw = require('../controller/shared').sharedTw;
+const linkedin = require('../controller/shared').sharedLink;
 
 router.get('/', function(req, res, next) {
     res.render('index', { tw: tw });
   });
 
 router.get('/whereAmI/:page', function(req, res, next) {
-    res.render(`whereAmI/${req.params.page}`, { tw: tw });
+    res.render(`whereAmI/${req.params.page}`, { tw: tw, linkedin: linkedin});
 });
 
 router.get('/cronograma/:page', function(req, res, next) {
-    res.render(`cronograma/${req.params.page}`, { tw: tw });
+    res.render(`cronograma/${req.params.page}`, { tw: tw, linkedin: linkedin });
 });
 
 router.get('/hall', function(req, res, next) {
-    res.render('hall/index', { tw: tw });
+    res.render('hall/index', { tw: tw, linkedin: linkedin });
 });
 
 router.get('/repo/:page', function(req, res, next) {
-    res.render(`repo/${req.params.page}`, { tw: tw });
+    res.render(`repo/${req.params.page}`, { tw: tw , linkedin: linkedin});
 });
 
 
